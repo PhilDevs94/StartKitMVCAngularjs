6518bcb163f3c4622763f8dcf398e3bf8e3862b9
PhilTran1994 <tranhoangphianh1994@gmail.com>
2020-01-07 18:04:25 +0700

    Update front end add some css and javascript files

--- StartMVCAngularJS.Web/Controllers/HomeController.cs
+++ StartMVCAngularJS.Web/Controllers/HomeController.cs
@@ -17,6 +17,9 @@ namespace StartMVCAngularJS.Web.Controllers
         {
             return PartialView();
         }
-
+        public ActionResult SideBar()
+        {
+            return PartialView();
+        }
     }
 }
