e7f4b4a01b3e7689ea5910c2f41d086dc5bccb5b
PhilTran1994 <tranhoangphianh1994@gmail.com>
2020-01-18 13:14:00 +0700

    Intergrated front-end into the project configue the router for now

--- StartMVCAngularJS.Web/App/app/mainCtrl.js
+++ StartMVCAngularJS.Web/App/app/mainCtrl.js
@@ -16,7 +16,7 @@
                 $('.nav.nav-sidebar .nav-active').removeClass('nav-active active');
                 $('.nav.nav-sidebar .active:not(.nav-parent)').closest('.nav-parent').addClass('nav-active active');
 
-                if($location.$$path == '/' || $location.$$path == '/layout-api'){
+                if ($location.$$path == '/' || $location.$$path == '/layout-api') {
                     $('.nav.nav-sidebar .nav-parent').removeClass('nav-active active');
                     $('.nav.nav-sidebar .nav-parent .children').removeClass('nav-active active');
                     if ($('body').hasClass('sidebar-collapsed') && !$('body').hasClass('sidebar-hover')) return;
@@ -24,10 +24,10 @@
                     $('.nav.nav-sidebar .nav-parent .children').slideUp(200);
                     $('.nav-sidebar .arrow').removeClass('active');
                 }
-                if($location.$$path == '/'){
+                if ($location.$$path == '/') {
                     $('body').addClass('dashboard');
                 }
-                else{
+                else {
                     $('body').removeClass('dashboard');
                 }
 
@@ -36,28 +36,5 @@
             $scope.isActive = function (viewLocation) {
                 return viewLocation === $location.path();
             };
-            // save settings to local storage
-            if (angular.isDefined($localStorage.settings)) {
-                $scope.newApp.settings = $localStorage.settings;
-            } else {
-                $localStorage.settings = $scope.app.settings;
-            }
-            $scope.$watch('newApp.settings', function () {
-                if ($scope.newApp.settings.asideDock && $scope.app.settings.asideFixed) {
-                    // aside dock and fixed must set the header fixed.
-                    $scope.newApp.settings.headerFixed = true;
-                }
-                // for box layout, add background image
-                $scope.newApp.settings.container ? angular.element('html').addClass('bg') : angular.element('html').removeClass('bg');
-                // save to local storage
-                $localStorage.settings = $scope.app.settings;
-            }, true);
-
-            function isSmartDevice($window) {
-                // Adapted from http://www.detectmobilebrowsers.com
-                var ua = $window['navigator']['userAgent'] || $window['navigator']['vendor'] || $window['opera'];
-                // Checks for iOs, Android, Blackberry, Opera Mini, and Windows mobile devices
-                return (/iPhone|iPod|iPad|Silk|Android|BlackBerry|Opera Mini|IEMobile/).test(ua);
-            }
 
         }]);
